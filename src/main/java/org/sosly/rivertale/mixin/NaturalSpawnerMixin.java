c61e788679a1c7b2e717b7387aeec1f637e2a291
Kevin Kragenbrink <kevin@writh.net>
2026-01-06 07:23:13 -0800

    feat: disable natural mob spawning in dev mode

---
+++ src/main/java/org/sosly/rivertale/mixin/NaturalSpawnerMixin.java
@@ -0,0 +1,34 @@
+package org.sosly.rivertale.mixin;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.NaturalSpawner;
+import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraftforge.fml.loading.FMLLoader;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(NaturalSpawner.class)
+public abstract class NaturalSpawnerMixin {
+
+    @Inject(
+        method = "spawnForChunk",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private static void skipMobSpawnsInDevMode(
+        ServerLevel level,
+        LevelChunk chunk,
+        NaturalSpawner.SpawnState spawnState,
+        boolean spawnFriendlies,
+        boolean spawnEnemies,
+        boolean forcedSpawn,
+        CallbackInfo ci
+    ) {
+        if (!FMLLoader.isProduction()) {
+            ci.cancel();
+        }
+    }
+}
